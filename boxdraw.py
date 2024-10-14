de20b5916eac7b1aa6a39426cc2262bed943ff6f
falky <falkor440@gmal.com>
2024-10-14 13:44:08 +0100

    why wont it draw a box

--- boxdraw.py
+++ boxdraw.py
@@ -1,10 +1,12 @@
 from enum import Enum
 
 cap = Enum, ['TOP', 'BOTTOM', 'MIDDLE', 'SINGLE']
-previous = ''
+
+boxb = ''
 
 
 def drawbox(boxcap: cap, x: int, y: int, cont: str, *div):
+    previous = []
     # This code is kinda a nightmare but what its doing is generating a top section, a mid section and a mid section with thext that will be displayed at the top line
     boxend = ''.join(('┏', ''.join(map(lambda i: i * x, '━')), '┓'))
     boxtxt = ''.join((f'┃{cont}', ''.join(map(lambda i: i * (x - len(cont)), ' ')), '┃'))
@@ -17,12 +19,26 @@ def drawbox(boxcap: cap, x: int, y: int, cont: str, *div):
             boxtxt = boxtxt[:div[i * 2]] + "┃" + div[i * 2 + 1] + boxtxt[div[i * 2] + len(div[i * 2 + 1]) + 1:]
             boxmid = boxmid[:div[i * 2]] + "┃" + boxmid[div[i * 2] + 1:]
 
+    def printtop(boxx: str):
+        boxx = boxx.replace('┏', '┣').replace('┓', '┫')
+        for k in range(len(previous[0]) - 2):
+            k += 1
+            if previous[1][k] == '┃':
+                boxx = boxx[:k] + "╋" + boxx[k + 1:]
+
+
+        print(boxx)
+
+
+
     def printmid():
         for i in range(y):
             if i == 0:
                 print(boxtxt)
             else:
                 print(boxmid)
+
+    previous = [boxend, boxmid]
 
     # Dont need to generate a bottom section, edit the top depending on case
     match boxcap:
@@ -34,10 +50,9 @@ def drawbox(boxcap: cap, x: int, y: int, cont: str, *div):
             print(boxend)
             printmid()
         case 'MIDDLE':
-
+            printtop(boxend)
             printmid()
         case 'BOTTOM':
+            printtop(boxend)
             printmid()
             print(boxend.replace('┏', '┗').replace('┓', '┛').replace('┳', '┻'))
-
-    previous = (boxcap, x, y, div)
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from boxdraw import drawbox
 
 x = "Lilith"
 y = "Witch"
-drawbox('TOP', False, 42+len(x), 2, f"Name: {x}", 30+len(x), "Level:")
-drawbox('MIDDLE', False, 42+len(x), 2, f"Class: {y}", 12+len(x), "Archytype")
-drawbox('BOTTOM', True, 42+len(x), 2, f"Name: {x}", 12+len(x), "Level:")
-drawbox('SINGLE', True, 42+len(x), 2, f"Name: {x}", 12+len(x), "Level:")
+drawbox('TOP', 42+len(x), 2, f"Name: {x}", 30+len(x), "Level:")
+drawbox('MIDDLE', 42+len(x), 2, f"Class: {y}", 12+len(x), "Archytype")
+drawbox('BOTTOM', 42+len(x), 2, f"Name: {x}", 12+len(x), "Level:")
+drawbox('SINGLE', 42+len(x), 2, f"Name: {x}", 12+len(x), "Level:")
 
 
 
