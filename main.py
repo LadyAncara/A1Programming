4d52b599ce9ec1697ad64e170b0cde06dd4ba87f
falky <falkor440@gmal.com>
2024-10-17 13:42:07 +0100

    its done

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from boxdraw import drawbox
 
-x = "Lilith"
-y = "Witch"
-prev = drawbox('TOP', [], 42+len(x), 1, f"Name: {x}", 30+len(x), "Level:")
-prev = drawbox('MIDDLE', prev, 42+len(x), 1, f"Class: {y}", 12+len(x), "Archytype")
-prev = drawbox('BOTTOM', prev, 42+len(x), 1, f"Name: {x}", 12+len(x), "Level:")
-drawbox('SINGLE', prev, 42+len(x), 1, f"Name: {x}", 12+len(x), "Level:")
+
+class Character:
+    stats = [0, 0, 0, 0, 0]
+
+    def __init__(self, name, arch, subarch, stats):
+        self.name = name
+        self.arch = arch
+        self.subarch = subarch
+        self.stats = stats
 
 
+statnames = ['DEX', 'STR', 'AGI', 'WIS', 'INT']
 
+Character.name = input("What is the name of your character? ")
+Character.arch = input("What is your class? ")
+Character.subarch = input("What is your subclass? ")
+print("What are your following stats? ")
+for i in range(5):
+    Character.stats[i] = int(input(f"What is your {statnames[i]}? "))
 
+statbonus = list(map(lambda x: int(x), list(map(lambda x: ((x - (x % 2)) / 2) - 5, Character.stats))))
+
+prev = drawbox('TOP', [], 42 + len(Character.name), 1, f"Name: {Character.name}", 30 + len(Character.name), "Level: 1")
+drawbox('BOTTOM', prev, 42 + len(Character.name), 1, f"Class: {Character.arch}", 12 + len(Character.arch),
+        f"Archetype: {Character.subarch}")
+
+prev = drawbox('TOP', [], 42, 1, "STAT", 14, "POINTS", 28, "PROF")
+for i in range(len(Character.stats) - 1):
+    drawbox('MIDDLE', prev, 42, 1, statnames[i], 14, str(Character.stats[i]), 28, str(statbonus[i]))
+drawbox('BOTTOM', prev, 42, 1, statnames[4], 14, str(Character.stats[4]), 28, str(statbonus[4]))
