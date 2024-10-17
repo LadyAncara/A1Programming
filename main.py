587a8d1ffdb662800951359d440f133f84b9eddf
falky <falkor440@gmal.com>
2024-10-17 13:54:39 +0100

    few extra comments including a TODO! that will probably never be TO DONE

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ Character.name = input("What is the name of your character? ")
 Character.arch = input("What is your class? ")
 Character.subarch = input("What is your subclass? ")
 print("What are your following stats? ")
+
+#TODO: make this not crash if the user doesnt put in numbers
 for i in range(5):
     Character.stats[i] = int(input(f"What is your {statnames[i]}? "))
 
+#gets the proficiency bonus from entred stat values
 statbonus = list(map(lambda x: int(x), list(map(lambda x: ((x - (x % 2)) / 2) - 5, Character.stats))))
 
+#uses drawbox() to format out haracter sheet to be nice and pretty
 prev = drawbox('TOP', [], 42 + len(Character.name), 1, f"Name: {Character.name}", 30 + len(Character.name), "Level: 1")
 drawbox('BOTTOM', prev, 42 + len(Character.name), 1, f"Class: {Character.arch}", 12 + len(Character.arch),
         f"Archetype: {Character.subarch}")
